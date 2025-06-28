8b5acda09316925c377d4873cff16b1a8a88dc22
DESKTOP-F47U60N\gabri <gabrielahercules94@gmail.com>
2025-06-28 16:25:07 -0600

    CRUD funcionando

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const reviewRoutes = require('./routes/reviewRoutes');
+
 
 app.use(cors());
 app.use(express.json());
-app.use('/reviews', reviewRoutes);
+
+//Rutas
+app.use('/api/review', require('./routes/reviewRoutes'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
