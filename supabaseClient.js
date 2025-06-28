627f0455135035472b5541ccf96481f91ab6a2c6
AVELAR\yesse <avelaryesser@gmail.com>
2025-06-28 14:17:28 -0600

    Creando archivo de testConecction para la prueba de conexion

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_SERVICE_ROLE
 )
+
 module.exports = supabase
---
+++ testConnectionSupabase.js
@@ -0,0 +1,12 @@
+const supabase = require('./supabaseClient')
+
+const testConnection = async () => {
+  const { data, error } = await supabase.auth.admin.listUsers()
+
+  if (error) {
+    console.log('Error de conexion')
+  } else {
+    console.log('Conexion exitosa')
+  }
+}
+testConnection()
